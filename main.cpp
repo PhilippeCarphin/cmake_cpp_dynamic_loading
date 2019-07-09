e9e7f44f9aeb0e42a6f08269fb59549084781705
Philippe Carphin <philippe.carphin@polymtl.ca>
2019-07-09 14:42:51 -0400

    Adding code to load plugin ...
    does not link because of
    
    undefined reference to `dlopen'
    undefined reference to `dlerror'

--- main.cpp
+++ main.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
+#include <dlfcn.h>
+#include "cmake_config.out.h"
 
 int main(void)
 {
   std::cerr << __PRETTY_FUNCTION__ << " ..." << std::endl;
+
+  std::string absolute_value_path;
+
+  absolute_value_path += std::string(PLUGIN_PATH) + "/AbsoluteValue/libAbsoluteValue.so";
+
+  void *plugin = dlopen(absolute_value_path.c_str(), RTLD_NOW);
+
+  if(plugin){
+      std::cerr << "Plugin loaded successfully" << std::endl;
+  } else {
+      std::cerr << "ERROR loading plugin : " << dlerror() << std::endl;
+  }
 
   return 0;
 }
