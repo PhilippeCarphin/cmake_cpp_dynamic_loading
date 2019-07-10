57eedba5dfce5c390c25db1e515a23eeff976ee6
Philippe Carphin <philippe.carphin@polymtl.ca>
2019-07-10 17:06:39 -0400

    Passing information from Python to C++ and back

--- pyspooki/pyspooki_interface.cpp
+++ pyspooki/pyspooki_interface.cpp
@@ -21,8 +21,24 @@ void pyspooki_interface_function(){
 }
 
 void internal_initializations(){
-    std::cerr << "C++      : This interface was compiled for loading by Python " << BOOST_PYTHON_VERSION << std::endl;
-    std::cerr << "C++      : This test is development on SPOOKI" << std::endl;
+    std::cerr << "C++      : " << __PRETTY_FUNCTION__ << std::endl;
+    std::cerr << "C++      : [info] This interface was compiled for loading by Python " << BOOST_PYTHON_VERSION << std::endl;
+    std::cerr << "C++      : [info] This test is development on SPOOKI" << std::endl;
+}
+
+const double e = 2.7182818284590452353602874713527;
+double sinh_impl(double x) {
+    return (1 - pow(e, (-2 * x))) / (2 * pow(e, -x));
+}
+
+double cosh_impl(double x) {
+    return (1 + pow(e, (-2 * x))) / (2 * pow(e, -x));
+}
+
+PyObject* tanh_impl(float x) {
+    std::cerr << "C++      : " << __PRETTY_FUNCTION__ << std::endl;
+    double tanh_x = sinh_impl(x) / cosh_impl(x);
+    return PyFloat_FromDouble(tanh_x);
 }
 
 using namespace boost::python;
@@ -31,6 +47,7 @@ BOOST_PYTHON_MODULE(libpyspooki_interface)
     class_<pyspooki_interface_class>("pyspooki_interface_class", init<>())
             .def("method", &pyspooki_interface_class::method);
     def("pyspooki_interface_function", pyspooki_interface_function);
+    def("tanh_impl", tanh_impl);
     internal_initializations();
 }
 
--- pyspooki/test_loading.py
+++ pyspooki/test_loading.py
@@ -1,9 +1,20 @@
+print()
+print("PYTHON   : Importing libpyspooki_interface")
 import libpyspooki_interface as interface
 
+print()
 print("PYTHON   : Creating an instance of a class")
 my_class = interface.pyspooki_interface_class()
+
+print()
 print("PYTHON   : Calling a method of the class")
 my_class.method()
 
+print()
 print("PYTHON   : Calling a regular exported function")
 interface.pyspooki_interface_function()
+
+print()
+print("PYTHON   : Calling interface.tanh_impl(2.0)")
+result = interface.tanh_impl(2.0)
+print("PYTHON   : Result of tanh_impl: {}".format(result))
